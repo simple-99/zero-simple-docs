d1a8d052895299652c5530036ccac2db0463f9b3
WeiShiHui <weish@zy-cast.com>
2024-12-17 13:31:45 +0800

    添加java线程扩展

--- pages/helper/nav.ts
+++ pages/helper/nav.ts
@@ -2,7 +2,7 @@
  * @version: 1.0
  * @Author: wsh
  * @Date: 2024-08-25 15:31:44
- * @LastEditTime: 2024-09-27 10:48:40
+ * @LastEditTime: 2024-12-17 13:31:02
  * @FilePath: \zero-simple-docs\pages\helper\nav.ts
  * @Description:
  */
@@ -44,6 +44,15 @@ export const nav = (): DefaultTheme.NavItem[] => {
         {
           link: "/tutorial/airport",
           text: "机场服务",
+        },
+        {
+          text: "Java基础",
+          items: [
+            {
+              link: "/java/thread",
+              text: "线程扩展",
+            },
+          ],
         },
       ],
     },
